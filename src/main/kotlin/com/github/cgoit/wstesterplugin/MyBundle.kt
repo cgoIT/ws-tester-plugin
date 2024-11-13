7e324047cec2acd159c5792ddd84bf9cc26b1897
GitHub Action <action@github.com>
2024-11-13 06:27:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/cgoit/wstesterplugin/MyBundle.kt
+++ src/main/kotlin/com/github/cgoit/wstesterplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cgoit.wstesterplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/cgoit/wstesterplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/cgoit/wstesterplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.cgoit.wstesterplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.cgoit.wstesterplugin.MyBundle
+import com.github.cgoit.wstesterplugin.services.MyProjectService
 import javax.swing.JButton
 
 
